065d7d5e1c565ebe38068498d00a658179650e11
itsabhi06 <abhiishekabhi06@gmail.com>
2025-08-11 02:52:13 +0530

    update backend.tf file

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "demo-resources"
+    resource_group_name  = "tf-resources"
     storage_account_name = "tfstorageaccount"
     container_name       = "dev-tfstate"
     key                  = "dev.terraform.tfstate"
